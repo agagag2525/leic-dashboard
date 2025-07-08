5e07100bee387c41ae62a8fab5a812e901eb652b
agagag2525 <agnespricillia219@gmail.com>
2025-07-08 12:13:17 +0700

    Add app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import logging
+
+logging.getLogger('streamlit.runtime.scriptrunner.script_run_context').setLevel(logging.ERROR)
+
+sheet_id = "1IsugMSq1iZuKw3fwkKsCc93MOqUsXG6HY5rvLZrQuqI"
+sheet_name = "Peserta"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+st.set_page_config(page_title="LEIC 2K25 Dashboard", layout="wide")
+st.title("🚛 Logistics Execution Industrial Challenge 2K25")
+st.subheader("📊 Real-Time Dashboard")
+
+try:
+    df = pd.read_csv(url, dtype=str).fillna("")
+    df = df.dropna(axis=1, how='all')
+
+    st.success("✅ Data berhasil dimuat!")
+
+    st.markdown("### 💰 Uang Per Tim")
+    if "Rank" in df.columns and "Uang" in df.columns:
+        st.dataframe(df[["Rank", "Uang"]])
+    else:
+        st.warning("Kolom 'Rank' dan/atau 'Uang' tidak ditemukan.")
+except Exception as e:
+    st.error(f"❌ Gagal memuat data: {e}")
+       
