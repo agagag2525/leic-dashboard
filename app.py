2fae91c99dbf835f200025d1611d29248fe9fe31
agagag2525 <agnespricillia219@gmail.com>
2025-07-21 06:40:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,26 @@ url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sh
 
 st.set_page_config(page_title="LEIC 2K25 Dashboard", layout="wide")
 
+base_img_url = "https://raw.githubusercontent.com/agagag2525/leic-dashboard/main/assets/"
+logo_paths = {
+    "WM": "Logo_WM_Standar_PNG.png",
+    "TI": "TI.png",
+    "HMPS": "HMPS.PNG",
+    "IC": "IC.png",
+    "KB": "Primary_Horizontal%20Logo.png"
+}
 col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1])
 
 with col1:
-    st.image("https://drive.google.com/uc?export=view&id=1LuA3DfdXXRXDiqFJpCiVjyJsMX0UXOsI", width=100)  # WM
+    st.image(base_img_url + logo_paths["WM"], width=100)
 with col2:
-    st.image("https://drive.google.com/uc?export=view&id=1XNy3M4hWvEfC7kHbCyWeoq5-IgHcwgbz", width=100)  # TI
+    st.image(base_img_url + logo_paths["TI"], width=100)
 with col3:
-    st.image("https://drive.google.com/uc?export=view&id=13Yms8QdLPh63OR0PUYxoTyA0H9YaHxjL", width=100)  # HMPS
+    st.image(base_img_url + logo_paths["HMPS"], width=100)
 with col4:
-    st.image("https://drive.google.com/uc?export=view&id=1dBrtaa64m8HcJ927N6J9W5to9eU00Ayj", width=100)  # IC
+    st.image(base_img_url + logo_paths["IC"], width=100)
 with col5:
-    st.image("https://drive.google.com/uc?export=view&id=1HijbSANYKVzavYqNkW7b64hokDmWm7kD", width=100)  # Kampus Berdampak
+    st.image(base_img_url + logo_paths["KB"], width=100)
 
 st.title("🚛 Logistics Execution Industrial Challenge 2K25")
 st.subheader("📊 Real-Time Dashboard")
