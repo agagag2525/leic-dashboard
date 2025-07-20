2979f71563bce7b846840701c2df4db7176b02b7
agagag2525 <agnespricillia219@gmail.com>
2025-07-21 06:20:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,20 @@ sheet_name = "Peserta"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
 st.set_page_config(page_title="LEIC 2K25 Dashboard", layout="wide")
+
+col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1.5])
+
+with col1:
+    st.image("https://drive.google.com/uc?export=view&id=1LuA3DfdXXRXDiqFJpCiVjyJsMX0UXOsI", width=80)  # Logo WM
+with col2:
+    st.image("https://drive.google.com/uc?export=view&id=1XNy3M4hWvEfC7kHbCyWeoq5-IgHcwgbz", width=80)  # Logo TI
+with col3:
+    st.image("https://drive.google.com/uc?export=view&id=13Yms8QdLPh63OR0PUYxoTyA0H9YaHxjL", width=80)  # Logo HMPS
+with col4:
+    st.image("https://drive.google.com/uc?export=view&id=1dBrtaa64m8HcJ927N6J9W5to9eU00Ayj", width=80)  # Logo IC
+with col5:
+    st.image("https://drive.google.com/uc?export=view&id=1HijbSANYKVzavYqNkW7b64hokDmWm7kD", width=120)  # Logo Kampus Berdampak
+
 st.title("🚛 Logistics Execution Industrial Challenge 2K25")
 st.subheader("📊 Real-Time Dashboard")
 
